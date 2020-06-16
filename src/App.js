b2190dc706304853fc3eeb8789ba9cad956bc0a2
MaharTariq <mahartariq499@gmail.com>
2020-06-16 12:16:33 +0500

    File Addition and Updation CP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Header} from './Components/Header';
+
 import './App.css';
+import { Balance } from './Components/Balance';
+import { IncomeExpenses } from './Components/IncomeExpenses';
+import { TransactionList } from './Components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+import {GlobalProvider} from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header/>
+      <div className="Container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/Components/TransactionList.js
@@ -0,0 +1,15 @@
+import React,{useContext} from 'react'
+import { GlobalContext} from '../context/GlobalState'
+import {Transaction} from './Transaction';
+
+export const TransactionList = () => {
+  const {transactions} = useContext(GlobalContext);  
+  return (
+        <React.Fragment>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction =>(<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+        </React.Fragment>
+    )
+}
